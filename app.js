42147882c82c21b634ea14efea341b7a789f3b35
Nicole <ni.ferng@gmail.com>
2025-08-06 16:00:37 -0400

    se agregan funciones para agregar amigos y resetear campo input

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [] ;
+
+function asignarTextoAUnElemento(element, text) {
+    let elementHTML = document.querySelector(element);
+    elementHTML.innerHTML = text;
+    return;
+}
+
+function agregarAmigo() {
+    let nombre = document.querySelector("#amigo").value;
+    if (nombre.trim().length === 0) {
+        alert("Por favor ingresa un nombre");
+    } else {
+        listaDeAmigos.push(nombre);
+        console.log(listaDeAmigos);
+        let contenedorLista = document.querySelector("#listaAmigos");    
+        contenedorLista.innerHTML = listaDeAmigos
+        .map(amigo => `<li>${amigo}</li>`)
+        .join('')
+        condicionesIniciales();
+    }
+}
+
+
+
+function condicionesIniciales() {
+    document.querySelector("#amigo").value = "";
+    return;
+}
