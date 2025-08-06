ef13f7d99ae888d26774629035f5cd7c924fc641
Nicole <ni.ferng@gmail.com>
2025-08-06 17:48:38 -0400

    se agregan funciones para sortear amigo secreto y cambio de color

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ function agregarAmigo() {
     let nombre = document.querySelector("#amigo").value;
     if (nombre.trim().length === 0) {
         alert("Por favor ingresa un nombre");
-    } else {
+    } else if (listaDeAmigos.some(amigo => amigo.toLowerCase() === nombre.toLowerCase())) {
+        alert("Ese nombre ya está en la lista.");
+    }
+    else {
         listaDeAmigos.push(nombre);
         console.log(listaDeAmigos);
         let contenedorLista = document.querySelector("#listaAmigos");    
@@ -23,8 +26,29 @@ function agregarAmigo() {
 }
 
 
-
 function condicionesIniciales() {
     document.querySelector("#amigo").value = "";
     return;
+}
+
+function validarSiHayAmigos() {
+    if (listaDeAmigos.length === 0) {
+        alert("No hay amigos en la lista");
+        return false;
+    } else {
+        return true;
+    }
+}
+function sortearAmigo() {
+    if (validarSiHayAmigos() === true) {
+    let amigoSorteado = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+    asignarTextoAUnElemento("h2", `Tu amigo secreto es <span style="color: #ff6730ff;"> ${amigoSorteado}</span>`);}
+    colorFondoTransicion();
+    document.querySelector('.button-draw').setAttribute('disabled', 'true');
+    document.querySelector('.button-draw').setAttribute('style', 'background-color: #adadadff; color: gray; cursor: not-allowed;');
+}
+
+function colorFondoTransicion() {
+    document.body.style.transition = "background-color 0.8s ease";
+    document.body.style.backgroundColor = "#ff6730ff";
 }
